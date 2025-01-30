34fb672d7e4efed2eb71a36aef5952b5069dd88c
satyamgupta09 <138917027+satyamgupta09@users.noreply.github.com>
2025-01-30 22:56:44 +0530

    Create Category.js

---
+++ src/components/Category.js
@@ -0,0 +1,26 @@
+import { Link, Outlet } from "react-router-dom";
+
+export default function Category() {
+  return (
+    <div>
+      <p>Women Items:</p>
+      <ul>
+        <li>
+          <Link to="grooming">Grooming</Link> {/* FIXED: Relative path */}
+        </li>
+        <li>
+          <Link to="shirt">Shirt</Link>
+        </li>
+        <li>
+          <Link to="trouser">Trouser</Link>
+        </li>
+        <li>
+          <Link to="jewellery">Jewellery</Link>
+        </li>
+      </ul>
+
+      <Outlet />
+      {/* This will render the selected item below the list */}
+    </div>
+  );
+}
